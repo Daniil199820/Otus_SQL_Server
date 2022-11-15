cc790960d3744921b3afbcdb4ea51fbb456318ea
Dannil199820 <dry.ginpost@gmail.com>
2022-11-15 23:50:57 +0300

    require command

--- Ditchr_Clients.h
+++ Ditchr_Clients.h
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <functional>
 
 class Client_require{
 public:
@@ -14,8 +15,13 @@ public:
         worker_thread = std::thread(&Client_require::process,this);
     }
 
-    void add_task(const std::string& str_){
+
+    void add_task(const std::string& command_str){
+        {
         std::lock_guard<std::mutex> guard(worker_mutex);
+        m_tasks.push(command_str);
+        }
+        wait_worker.notify_all();
     }
 
 
@@ -24,11 +30,15 @@ private:
     std::atomic<bool> m_stopped;
     std::mutex worker_mutex;
     std::condition_variable wait_worker;
-    std::queue<std::pair<TTask, TCallback>> m_tasks;
+    std::queue<std::string> m_tasks;
 
     void process(){
         while(m_stopped==false){
-            
+            std::unique_lock<std::mutex> locker(worker_mutex);
+            wait_worker.wait(locker,[&](){return !m_tasks.empty()||m_stopped==true;});
+            std::string temp_command = m_tasks.front();
+            m_tasks.pop();
+            locker.unlock();
         }   
     }
 };
